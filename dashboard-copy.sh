00987599765bb72043e84e0c7d98fc4feebb3cfb
Radoslaw Cieciwa <radoslaw.cieciwa@corp.badoo.com>
2018-10-03 22:05:15 +0100

    Added review feature.

--- dashboard-copy.sh
+++ dashboard-copy.sh
@@ -6,17 +6,59 @@
 
 
 function usage() {
-  echo "Usage: $0 <[add; clear]>"
+  echo "Usage: $0 [add; clear; review]"
+  exit 1
 }
 
-if [ "$#" -ne 1 ]; then
-    usage
-    exit 1
-fi
+function usage_review() {
+  echo "Usage: $0 review <ticket>"
+  exit 1
+}
 
 COPY_NAME="tmp_working_copy"
-MAIN_WORKSPACE_ROOT=/Users/radoslawcieciwa/Development/iOS/Badoo
+export REVIEW_DIRECTORY_NAME="REVIEW"
+export MAIN_WORKSPACE_ROOT=/Users/radoslawcieciwa/Development/iOS/Badoo
 MAIN_REPO_PATH=/Users/radoslawcieciwa/Development/iOS/Badoo/_source
+export DIR=$(dirname $0)
+
+function remove_wortree() {
+  if [ $# -ne 2 ]; then
+    echo "Wrong usage try: ${FUNCNAME[0]} <DIRECTORY_ROOT> <WORKTREE>"
+    return 1
+  fi
+
+  git -C "$1/$2" reset --hard
+  git -C "$1/$2" clean -fdx
+
+  $DIR/dashboard-ticket-delete.sh $2
+  if [ -d "$1/$2" ]; then
+    rm -r $2
+  fi
+}
+
+export -f remove_wortree
+
+function qcheckout() {
+	if [ $# -ne 1 ]; then
+		echo "Wrong usage try: ${FUNCNAME[0]} <START_OF_BRANCH_NAME>"
+		return 1
+	fi
+
+	START_OF_BRANCH_NAME=$1
+	echo "Looking for: $START_OF_BRANCH_NAME ... in $MAIN_REPO_PATH"
+
+	REMOTE=`git -C $MAIN_REPO_PATH branch -a | grep $1`
+	FOUND_LINES=`git -C $MAIN_REPO_PATH branch -a | grep $1 | wc -l | sed -e 's/^[[:space:]]*//'`
+
+	if [ $FOUND_LINES -ne 1 ]; then
+		echo "ERROR: Found $FOUND_LINES branches with this signature"
+		return 1
+	fi
+
+	LOCAL="$START_OF_BRANCH_NAME${REMOTE#*$START_OF_BRANCH_NAME}"
+	echo "Executing: git checkout -b $LOCAL $REMOTE"
+  git -C $MAIN_REPO_PATH worktree add --checkout "$MAIN_WORKSPACE_ROOT/$REVIEW_DIRECTORY_NAME/$1" $REMOTE
+}
 
 if [ "$1" = "add" ]; then
   if [ -d "$MAIN_WORKSPACE_ROOT/$COPY_NAME" ]; then
@@ -32,14 +74,15 @@ elif [ "$1" = "clear" ]; then
     exit 0
   fi
 
-  git -C $MAIN_REPO_PATH reset --hard
-  git -C $MAIN_REPO_PATH clean -fdx
-
-  DIR=$(dirname $0)
-  $DIR/dashboard-ticket-delete.sh $COPY_NAME
-  if [ -d "$MAIN_WORKSPACE_ROOT/$COPY_NAME" ]; then
-    rm -r $COPY_NAME
+  remove_wortree $MAIN_WORKSPACE_ROOT $COPY_NAME
+elif [ "$1" = "review" ]; then
+  if [ "$#" -eq 2 ]; then
+    qcheckout $2
+  else
+    echo "Clearing all reviews ..."
+    ls -1 $MAIN_WORKSPACE_ROOT/$REVIEW_DIRECTORY_NAME | xargs -L1 bash -c 'remove_wortree "$MAIN_WORKSPACE_ROOT/$REVIEW_DIRECTORY_NAME" $0'
   fi
 else
+  echo "$1"
   usage
 fi
