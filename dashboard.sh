5911998ea7f98f33ff89fdb3bb9a367d675a2f08
lem <lemsergey@gmail.com>
2019-02-26 13:26:41 +0000

    Make list of tickets command portable

--- dashboard.sh
+++ dashboard.sh
@@ -9,7 +9,7 @@ export TICKETS_WORKSPACE_DIR="$BADOO_REPO_DIR"
 export SOURCE_REPO_PATH="$BADOO_REPO_DIR/_source"
 
 function query_LIST_OF_REPOS() {
-  LIST_OF_REPOS=`git -C $SOURCE_REPO_PATH worktree list | awk '{ print $1 }' | grep -v $SOURCE_REPO_PATH | grep -v "tmp_working_copy" | cut -d '/' -f 7 | awk 'ORS=","' | sed 's/\(.*\),/\1 /'`
+  LIST_OF_REPOS=`git -C $SOURCE_REPO_PATH worktree list | awk '{ print $1 }' | grep -v $SOURCE_REPO_PATH | sed 's#.*/##' | awk 'ORS=","' | sed 's/\(.*\),/\1 /'`
 }
 
 function usage() {
