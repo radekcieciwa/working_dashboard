cb8f434668930397551b5f8af1ca36912ba8c962
Radek Cieciwa <radekcieciwa@gmail.com>
2019-03-23 23:08:02 +0000

    Updated force delete.

--- dashboard-ticket-delete.sh
+++ dashboard-ticket-delete.sh
@@ -6,43 +6,61 @@
 
 function usage() {
   echo "usage dashboard delete <jira ticket>"
+  echo
+  echo "  -f to force delete"
 }
 
-if [ "$#" -ne 1 ]; then
+function delete_worktree_checkout() {
+  if [ "$FORCE_CLEANUP" = "1" ]; then
+    rm -rf $1
+    git -C $SOURCE_REPO_PATH worktree prune
+    git -C $SOURCE_REPO_PATH worktree remove --force $1
+  else
+    git -C $SOURCE_REPO_PATH worktree remove $1
+  fi
+}
+
+function delete_branch() {
+  if [ "$FORCE_CLEANUP" = "1" ]; then
+    git -C $SOURCE_REPO_PATH branch --force -D $1
+  else
+    git -C $SOURCE_REPO_PATH branch -d $1
+  fi
+}
+
+TICKET=$1
+FORCE_CLEANUP=0
+
+while getopts f OPTION
+do
+    case ${OPTION} in
+        f)  FORCE_CLEANUP=1
+            ;;
+    esac
+done
+
+TICKET=${@:$OPTIND:1}
+
+if [ -z "$TICKET" ]; then
     usage
     exit 1
 fi
 
-WORKING_COPY=$1
-FORCE_CLEANUP=0
-
-echo "Start"
-echo "Removing $WORKING_COPY..."
+echo "Deleting $TICKET ..."
 
 if [ "$FORCE_CLEANUP" = "1" ]; then
-  rm -rf $WORKING_COPY
-  git -C $SOURCE_REPO_PATH worktree prune
+  echo "Warning - using force flag"
 fi
 
-git -C $SOURCE_REPO_PATH worktree remove $WORKING_COPY
+delete_worktree_checkout $TICKET
 
-git -C $SOURCE_REPO_PATH branch | grep $WORKING_COPY | while read -r branch ; do
-  if [ "$FORCE_CLEANUP" = "1" ]; then
-    echo "Force removing $branch branch ..."
-    git -C $SOURCE_REPO_PATH reset --hard
-    git -C $SOURCE_REPO_PATH clean -fdx # to remove all untracked files
-    git -C $SOURCE_REPO_PATH branch --force -D $branch
-    echo "OK"
-  else
-    echo "Removing $branch branch ..."
-    git -C $SOURCE_REPO_PATH branch -d $branch
-    echo "OK"
-  fi
+git -C $SOURCE_REPO_PATH branch | grep $TICKET | while read -r branch ; do
+  delete_branch $branch
 done
 
 if [ $? -ne 0 ]; then
-  echo "$WORKING_COPY cleaning completed with error $?"
+  echo "$TICKET Finished with error $?"
   exit 1
 else
-  echo "$WORKING_COPY cleaning done!"
+  echo "$TICKET Done"
 fi
